242c9c110425071f99dd5597b58111e6506e119c
www222W <148997033+www222W@users.noreply.github.com>
2023-10-25 18:19:39 +0200

    Add files via upload

---
+++ kosmos.js
@@ -0,0 +1,6 @@
+// JavaScript można dodać, aby dodać bardziej zaawansowane funkcje, takie jak pobieranie danych o kosmosie z API lub animacje kosmiczne.
+
+// Przykład prostego kodu JavaScript:
+document.addEventListener("DOMContentLoaded", function () {
+    // Możesz dodać swoje skrypty JavaScript tutaj.
+});
